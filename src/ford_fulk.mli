d3afada1a8fa0e1c277acff1afdfada1a724833f
clemcloummm <clement.ospital64@gmail.com>
2026-01-06 21:51:45 +0100

    Fonction min .mli

--- src/ford_fulk.mli
+++ src/ford_fulk.mli
@@ -7,5 +7,8 @@ val find_path : 'a graph -> id -> id -> id list
 (*Takes a list of id and return all the arcs that the algorithm took to go to the dest node*)
 val find_arc_path : 'a graph -> id list -> 'a arc list
 
+(*Returns the minimum capacity of an arc path*)
+val find_min_capacity : 'a arc list -> int
+
 (*Returns the maximum flows value of a graph applying the Ford-Fulkerson algorithm*)
 val algo_ford : 'a graph -> id -> id -> 'a
